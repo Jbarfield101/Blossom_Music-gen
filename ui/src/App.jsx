1d7c56b23e24d53e30cfc92b0f175053a19ba6ff
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-14 02:16:33 -0400

    Add reusable BackButton component

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -10,23 +10,24 @@ import Generate from './pages/Generate.jsx';
 import MusicGenerator from './pages/MusicGenerator.jsx';
 import MusicLang from './pages/MusicLang.jsx';
 import MusicGen from './pages/MusicGen.jsx';
+import BackButton from './components/BackButton.jsx';
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/music-generator" element={<MusicGenerator />} />
-        <Route path="/music-generator/algorithmic" element={<Generate />} />
-        <Route path="/music-generator/musiclang" element={<MusicLang />} />
-        <Route path="/music-generator/musicgen" element={<MusicGen />} />
-        <Route path="/generate" element={<Generate />} />
-        <Route path="/dnd" element={<Dnd />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/profiles" element={<Profiles />} />
-        <Route path="/train" element={<Train />} />
-        <Route path="/onnx" element={<OnnxCrafter />} />
-        <Route path="/models" element={<Models />} />
+        <Route path="/music-generator" element={<><BackButton /><MusicGenerator /></>} />
+        <Route path="/music-generator/algorithmic" element={<><BackButton /><Generate /></>} />
+        <Route path="/music-generator/musiclang" element={<><BackButton /><MusicLang /></>} />
+        <Route path="/music-generator/musicgen" element={<><BackButton /><MusicGen /></>} />
+        <Route path="/generate" element={<><BackButton /><Generate /></>} />
+        <Route path="/dnd" element={<><BackButton /><Dnd /></>} />
+        <Route path="/settings" element={<><BackButton /><Settings /></>} />
+        <Route path="/profiles" element={<><BackButton /><Profiles /></>} />
+        <Route path="/train" element={<><BackButton /><Train /></>} />
+        <Route path="/onnx" element={<><BackButton /><OnnxCrafter /></>} />
+        <Route path="/models" element={<><BackButton /><Models /></>} />
       </Routes>
     </>
   );
