ad4c57a98aaaf39903e98045ce2330de93adfdb9
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-14 14:27:46 -0400

    Remove redundant algorithmic and phrase routes

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -8,7 +8,6 @@ import AlgorithmicGenerator from './pages/Generate.jsx';
 import MusicGenerator from './pages/MusicGenerator.jsx';
 import MusicLang from './pages/MusicLang.jsx';
 import MusicGen from './pages/MusicGen.jsx';
-import PhraseModel from './pages/PhraseModel.jsx';
 
 export default function App() {
   return (
@@ -16,8 +15,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/music-generator" element={<MusicGenerator />} />
-        <Route path="/music-generator/algorithmic" element={<AlgorithmicGenerator />} />
-        <Route path="/music-generator/phrase" element={<PhraseModel />} />
         <Route path="/music-generator/musiclang" element={<MusicLang />} />
         <Route path="/music-generator/musicgen" element={<MusicGen />} />
         <Route path="/generate" element={<AlgorithmicGenerator />} />
