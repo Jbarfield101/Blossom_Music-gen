453e79220ed16cccbaf608ce7323f769701e0452
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-13 21:36:20 -0400

    update devicesjs

--- ui/src/api/devices.js
+++ ui/src/api/devices.js
@@ -1,4 +1,5 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/core";
+import { open, save } from "@tauri-apps/plugin-dialog";
 
 export const listDevices = () => invoke("list_devices");
 export const setDevices = ({ input, output }) =>
