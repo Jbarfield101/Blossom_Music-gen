f28ac31154b44cc18db469514eb1c458c50ef9b8
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-15 01:45:00 -0400

    feat: add games page

---
+++ ui/src/pages/Games.jsx
@@ -0,0 +1,15 @@
+import BackButton from '../components/BackButton.jsx';
+import Card from '../components/Card.jsx';
+
+export default function Games() {
+  return (
+    <>
+      <BackButton />
+      <h1>Games</h1>
+      <main className="dashboard">
+        <Card to="/games/rain-blocks" icon="Blocks" title="Rain Blocks" />
+        <Card to="/games/snake" icon="Worm" title="Snake" />
+      </main>
+    </>
+  );
+}
