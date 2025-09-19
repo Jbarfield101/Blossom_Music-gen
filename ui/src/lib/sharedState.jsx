cf7a5d29a74028ec068d6362acd008a359b93d77
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-19 00:52:51 -0400

    Add loop maker output format selection

--- ui/src/lib/sharedState.jsx
+++ ui/src/lib/sharedState.jsx
@@ -22,6 +22,7 @@ export const DEFAULT_MUSICGEN_FORM = Object.freeze({
 export const DEFAULT_LOOPMAKER_FORM = Object.freeze({
   targetSeconds: 3600,
   targetInput: '3600',
+  outputFormat: 'video/webm;codecs=vp9,opus',
 });
 
 export const DEFAULT_BEATMAKER_FORM = Object.freeze({
