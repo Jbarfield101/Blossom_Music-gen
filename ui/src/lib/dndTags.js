5aea304693678cbab21fb36ff0bd1d1396859176
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-22 23:33:37 -0400

    Add DnD tag manager view and shared tag list

---
+++ ui/src/lib/dndTags.js
@@ -0,0 +1,19 @@
+export const TAGS = [
+  'NPCs',
+  'Pantheon',
+  'Monsters',
+  'Quests',
+  'Factions',
+  'Regions',
+  'Locations',
+  'Items',
+  'Events',
+  'Lore',
+  'Rumors',
+  'Encounters',
+  'Treasures',
+  'Downtime',
+  'Worldbuilding',
+];
+
+export default TAGS;
