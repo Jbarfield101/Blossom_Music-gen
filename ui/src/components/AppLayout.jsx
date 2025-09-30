53597d912e3096c57679a4c4676f23a6b909b067
Jbarfield101 <Jbarfield101@gmail.com>
2025-09-30 16:13:42 -0400

    Conditionally render main navigation on dashboard

--- ui/src/components/AppLayout.jsx
+++ ui/src/components/AppLayout.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import MainNav from './MainNav.jsx';
 
 export default function AppLayout() {
+  const location = useLocation();
+  const normalizedPath = location.pathname.replace(/\/+$/, '') || '/';
+  const showNav = normalizedPath !== '/';
+
   return (
     <div className="app-layout">
       <a className="skip-link" href="#main-content">
         Skip to content
       </a>
-      <MainNav />
+      {showNav && <MainNav />}
       <main id="main-content" className="app-layout__content" tabIndex={-1}>
         <Outlet />
       </main>
